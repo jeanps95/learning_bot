28063b6f57e9af96dab368d5e05b1caffbef0903
Jean Paulo Silva <jeanps95@gmail.com>
2020-01-14 14:23:54 -0300

    Additional information

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/jean/learning_robot_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/jean/learning_robot_ws/devel;/opt/ros/melodic".split(';') if "/home/jean/learning_robot_ws/devel;/opt/ros/melodic" != "" else []
---
+++ build/learning_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jean/learning_robot_ws/src/learning_robot
+# Build directory: /home/jean/learning_robot_ws/build/learning_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
