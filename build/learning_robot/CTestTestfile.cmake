3d56de8ee9b4b676e61f1ab9395588acde8a3f8a
Jean Paulo Silva <jeanps95@gmail.com>
2020-01-14 14:24:55 -0300

    removed build devel

--- build/learning_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jean/learning_robot_ws/src/learning_robot
-# Build directory: /home/jean/learning_robot_ws/build/learning_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/robot_diff_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jean/learning_robot_ws/src/robot_diff_gazebo
-# Build directory: /home/jean/learning_robot_ws/build/robot_diff_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
